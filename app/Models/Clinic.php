c0fe514615fe88943460b75b2475513313c11174
Abdullah Dheir <tareqm577@gmail.com>
2026-05-30 05:58:13 +0300

    إضافة HasMedia trait إلى Clinic model
    - إضافة HasMedia interface و InteractsWithMedia trait
    - إصلاح خطأ SpatieMediaLibraryFileUpload في Filament

--- app/Models/Clinic.php
+++ app/Models/Clinic.php
@@ -8,11 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 #[Fillable(['name', 'address', 'phone', 'logo_url', 'manager_id', 'working_hours', 'is_active'])]
-class Clinic extends Model
+class Clinic extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
 
     protected $casts = [
         'working_hours' => 'array',
